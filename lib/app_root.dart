ede301a72c9c8f42465f58914cd410cc129c9cd5
Coby-Amar <cobyamar@gmail.com>
2023-12-13 14:57:48 +0200

    skeleton set

---
+++ lib/app_root.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppRoot extends StatelessWidget {
+  const AppRoot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:organizer_tracker_app/app_root.dart';
+
+void main() {
+  runApp(const AppRoot());
+}
